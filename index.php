4b249eebf586d92a39aafcb664f939e4d4a9990f
lucca660 <ciolilucca@gmail.com>
2025-11-29 18:46:00 +0100

    h

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title></title>
+  <title>Clipsou Streaming - Acceuil</title>
   </head>
 <body>
-
+<header>
+    <h1>Clipsou Streaming</h1>
+</header>
 </body>
+<footer>
+    <p>Clipsou Streaming - 2025</p>
+</footer>
 </html>
